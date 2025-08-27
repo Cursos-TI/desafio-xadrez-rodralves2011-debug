1cf9ba5a947137984ab7fc8bfedb6a3c9349eaca
Felipe-Alves <rodralves2011@gmail.com>
2025-08-27 23:46:42 +0000

    segundo-commit-for

--- xadrez.c
+++ xadrez.c
@@ -9,6 +9,17 @@ int main(void) {
     const int MOVIMENTOS_RAINHA = 8;
     int i = 0; // Variável de controle para os laços
 
+    printf("--- SIMULACAO DE MOVIMENTACAO DE PECAS ---\n");
+
+   
+    // --- MOVIMENTO DA TORRE: USANDO O LAÇO FOR ---
+    // A Torre se move 5 casas para a direita.
+    // A função do for é executar um bloco de código repetidamente, como fazer "um monte de coisa" várias vezes, para cada item em uma lista ou para contar até um certo número.
+   
+    printf("\n>>> Movimento da TORRE (5 casas para a Direita)\n");
+    for (i = 0; i < MOVIMENTOS_TORRE; i++) {
+        printf("Direita\n");
+    }
    
     return 0;
 }
